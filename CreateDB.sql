5f66d3a1ab1eb880e220c6aa546b363cbbfe199c
master <fedushov@mail.ru>
2015-11-15 23:33:18 +0600

    more correct configure db

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,7 +1,4 @@
-CREATE DATABASE hellorails_development;
-CREATE DATABASE hellorails_production;
-CREATE DATABASE hellorails_test;
 CREATE USER rails WITH PASSWORD '123456';
-GRANT ALL PRIVILEGES ON DATABASE hellorails_development TO rails;
-GRANT ALL PRIVILEGES ON DATABASE hellorails_production TO rails;
-GRANT ALL PRIVILEGES ON DATABASE hellorails_test TO rails;
+CREATE DATABASE hellorails_development OWNER rails;
+CREATE DATABASE hellorails_production OWNER rails;
+CREATE DATABASE hellorails_test OWNER rails;
